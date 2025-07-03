1cf83e0064d0b6048376532b6e69a0fecfacdd34
uabdulma1ik <uraimovabdulmalik7@gmail.com>
2025-07-03 19:02:27 +0500

    .

--- lib/logic/note/note_bloc.dart
+++ lib/logic/note/note_bloc.dart
@@ -4,7 +4,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:equatable/equatable.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:habit/data/note_model.dart';
-
 part 'note_event.dart';
 part 'note_state.dart';
 
